81c68a4cb0c5ce68b34fa9eeb95bcdbac21781fa
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2024-05-23 13:26:42 +0200

    FIxed Account UI/Model

--- src/app/core/user/user.types.ts
+++ src/app/core/user/user.types.ts
@@ -1,21 +1,15 @@
-export class Image {
-    public_id: string;
-    url: string;
-}
-
 export class User {
+    id: string;
     name: string;
     email: string;
-    phoneNumber: string;
-    password: string;
-    createdAt: Date;
     avatar?: string;
-    image: string;
-    Role: string;
-    Fonction: string;
-    resetLink: {
-        data: string;
-    };
-    status: string;
+    status?: string;
+    phoneNumber: string;
     description: string;
+    role: string;
+    address: String;
+    birthDate: String;
+    codePostal: String;
+    country: String;
+    city: String;
 }
