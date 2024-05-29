880e21d9f7351acae8f39b9ce744dcb46f753ae8
ikbel436 <61362647+ikbel436@users.noreply.github.com>
2024-05-29 13:37:21 +0100

    changes

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -4,25 +4,25 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 export const defaultNavigation: FuseNavigationItem[] = [
     {
         id: 'exemple',
-        title: 'Your projects',
+       
         type: 'group',
         icon: 'heroicons_outline:home',
         children: [
 
-            {
-                id: 'dashboards.project',
-                title: 'Create Project',
-                type: 'basic',
-                icon: 'heroicons_outline:square-3-stack-3d',
-                link: '/createProject',
-            },
-            {
-                id: 'dashboards.project',
-                title: 'Git Provider ',
-                type: 'basic',
-                icon: 'heroicons_outline:code-bracket-square',
-                link: '/gitProvider',
-            },
+            // {
+            //     id: 'dashboards.project',
+            //     title: 'Create Project',
+            //     type: 'basic',
+            //     icon: 'heroicons_outline:square-3-stack-3d',
+            //     link: '/createProject',
+            // },
+            // {
+            //     id: 'dashboards.project',
+            //     title: 'Git Provider ',
+            //     type: 'basic',
+            //     icon: 'heroicons_outline:code-bracket-square',
+            //     link: '/gitProvider',
+            // },
 
 
 
@@ -37,73 +37,73 @@ export const defaultNavigation: FuseNavigationItem[] = [
 
         ],
     },
-    {
-        id: 'User',
-        title: 'Personal Informations',
-        type: 'group',
-        icon: 'heroicons_outline:home',
-        children: [
-            {
-                id: 'user.profile',
-                title: 'Profile',
-                type: 'basic',
-                icon: 'heroicons_outline:user-circle',
-                link: '/profile',
-            },
-            {
-                id: 'user.profile',
-                title: 'Settings',
-                type: 'basic',
-                icon: 'heroicons_outline:user-circle',
-                link: '/settings',
-            },
-        ]
-    },
-    {
-        id: 'apps',
-        title: 'Services',
-        subtitle: 'Developers Self-Service',
-        type: 'group',
-        icon: 'heroicons_outline:home',
-        children: [
-            {
-                id: 'user-interface.cards',
-                title: 'Configure Infrastructure',
-                type: 'basic',
-                icon: 'heroicons_outline:cloud',
-                link: 'configInfrastructure',
-            },
-            {
-                id: 'user-interface.cards',
-                title: 'Cloud Infrastructure Templates',
-                type: 'basic',
-                icon: 'heroicons_outline:cloud',
-                link: 'templateTerraform',
-            },
-            {
-                id: 'user-interface.cards',
-                title: 'Download Terrafrom Templates',
-                type: 'basic',
-                icon: 'heroicons_outline:cloud',
-                link: '/ui/cards',
-            },
-            {
-                id: 'user-interface.cards',
-                title: 'Gitlab CI Templates',
-                type: 'basic',
-                icon: 'heroicons_outline:cloud',
-                link: '/ui/cards',
-            },
-            {
-                id: 'user-interface.cards',
-                title: 'Create your infrastructure',
-                type: 'basic',
-                icon: 'heroicons_outline:cloud',
-                link: '/ui/forms/layouts',
-            },
+//     {
+//         id: 'User',
+//         title: 'Personal Informations',
+//         type: 'group',
+//         icon: 'heroicons_outline:home',
+//         children: [
+//             {
+//                 id: 'user.profile',
+//                 title: 'Profile',
+//                 type: 'basic',
+//                 icon: 'heroicons_outline:user-circle',
+//                 link: '/profile',
+//             },
+//             {
+//                 id: 'user.profile',
+//                 title: 'Settings',
+//                 type: 'basic',
+//                 icon: 'heroicons_outline:user-circle',
+//                 link: '/settings',
+//             },
+//         ]
+//     },
+//     {
+//         id: 'apps',
+//         title: 'Services',
+//         subtitle: 'Developers Self-Service',
+//         type: 'group',
+//         icon: 'heroicons_outline:home',
+//         children: [
+//             {
+//                 id: 'user-interface.cards',
+//                 title: 'Configure Infrastructure',
+//                 type: 'basic',
+//                 icon: 'heroicons_outline:cloud',
+//                 link: 'configInfrastructure',
+//             },
+//             {
+//                 id: 'user-interface.cards',
+//                 title: 'Cloud Infrastructure Templates',
+//                 type: 'basic',
+//                 icon: 'heroicons_outline:cloud',
+//                 link: 'templateTerraform',
+//             },
+//             {
+//                 id: 'user-interface.cards',
+//                 title: 'Download Terrafrom Templates',
+//                 type: 'basic',
+//                 icon: 'heroicons_outline:cloud',
+//                 link: '/ui/cards',
+//             },
+//             {
+//                 id: 'user-interface.cards',
+//                 title: 'Gitlab CI Templates',
+//                 type: 'basic',
+//                 icon: 'heroicons_outline:cloud',
+//                 link: '/ui/cards',
+//             },
+//             {
+//                 id: 'user-interface.cards',
+//                 title: 'Create your infrastructure',
+//                 type: 'basic',
+//                 icon: 'heroicons_outline:cloud',
+//                 link: '/ui/forms/layouts',
+//             },
 
-        ],
-    },
+//         ],
+//     },
 ];
 export const compactNavigation: FuseNavigationItem[] = [
     {
