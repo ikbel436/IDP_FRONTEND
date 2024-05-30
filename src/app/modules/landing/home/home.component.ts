711322dd997af06ce7741b54626df22a74217ed1
nesrine-snoussi <nesrine.snoussi@esprit.tn>
2024-05-30 17:56:30 +0100

    landing page

--- src/app/modules/landing/home/home.component.ts
+++ src/app/modules/landing/home/home.component.ts
@@ -1,21 +1,28 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Subject, takeUntil } from 'rxjs';
+import { NgClass, NgIf } from '@angular/common';
+import { FuseCardComponent } from '@fuse/components/card';
 
 @Component({
-    selector     : 'landing-home',
-    templateUrl  : './home.component.html',
+    selector: 'landing-home',
+    templateUrl: './home.component.html',
     encapsulation: ViewEncapsulation.None,
-    standalone   : true,
-    imports      : [MatButtonModule, RouterLink, MatIconModule],
+    standalone: true,
+    imports: [MatButtonModule, FuseCardComponent, RouterLink, MatIconModule, MatExpansionModule, MatFormFieldModule, MatInputModule, NgIf, NgClass],
 })
-export class LandingHomeComponent
-{
+export class LandingHomeComponent {
+
+    yearlyBilling: boolean = true;
     /**
      * Constructor
      */
-    constructor()
-    {
+    constructor() {
     }
 }
