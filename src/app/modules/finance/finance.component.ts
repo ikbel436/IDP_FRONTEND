d7def0fdb2c311a742c10bd429b0a54d7a35b73a
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2024-07-10 15:43:29 +0200

    bugs fixed

--- src/app/modules/finance/finance.component.ts
+++ src/app/modules/finance/finance.component.ts
@@ -37,7 +37,11 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CreateDeploymentComponent } from '../create-deployment/create-deployment.component';
 import { FuseLoadingBarComponent } from '@fuse/components/loading-bar';
 import { RouterModule } from '@angular/router';
-import { IStepOption, TourMatMenuModule, TourService } from 'ngx-ui-tour-md-menu';
+import {
+    IStepOption,
+    TourMatMenuModule,
+    TourService,
+} from 'ngx-ui-tour-md-menu';
 
 export interface Repository {
     name: string;
@@ -75,44 +79,52 @@ export interface Repository {
         MatFormFieldModule,
         GetProjectsComponent,
         MatCheckboxModule,
-        FuseLoadingBarComponent,RouterModule,TourMatMenuModule
+        FuseLoadingBarComponent,
+        RouterModule,
+        TourMatMenuModule,
     ],
 })
 export class FinanceComponent implements OnInit, AfterViewInit, OnDestroy {
     public readonly tourService = inject(TourService);
     hasStartedTour = false;
-   readonly tourSteps: IStepOption[] = [
-  {
-    anchorId: 'home',
-    content: 'Discover the power of our Git provider integration! Easily import your repositories and start managing your projects with ease.',
-    title: 'Repositories Import',
-    enableBackdrop: true,
-  },
-  {
-    anchorId: 'about',
-    content: 'Add new projects to your workspace with just a few clicks. Streamline your development process and stay on top of your project portfolio.',
-    title: 'Add New Projects',
-    enableBackdrop: true,
-  },
-  {
-    anchorId: 'contact',
-    content: 'Select the projects you want to bundle together. Consolidate your efforts and optimize your deployment workflow.',
-    title: 'Select Projects',
-    enableBackdrop: true,
-  },
-  {
-    anchorId: 'bundle',
-    content: 'Create custom bundles from the selected projects. Group related components, services, and configurations to simplify your deployment process.',
-    title: 'Create Bundles',
-    enableBackdrop: true,
-  },
-  {
-    anchorId: 'bundles',
-    content: 'Choose a bundle from the list and deploy your projects with a single click. Streamline your application delivery and ensure consistent deployments.',
-    title: 'Select Bundle',
-    enableBackdrop: true,
-  },
-];
+    showGitProviderPopup = false;
+    readonly tourSteps: IStepOption[] = [
+        {
+            anchorId: 'home',
+            content:
+                'Discover the power of our Git provider integration! Easily import your repositories and start managing your projects with ease.',
+            title: 'Repositories Import',
+            enableBackdrop: true,
+        },
+        {
+            anchorId: 'about',
+            content:
+                'Add new projects to your workspace with just a few clicks. Streamline your development process and stay on top of your project portfolio.',
+            title: 'Add New Projects',
+            enableBackdrop: true,
+        },
+        {
+            anchorId: 'contact',
+            content:
+                'Select the projects you want to bundle together. Consolidate your efforts and optimize your deployment workflow.',
+            title: 'Select Projects',
+            enableBackdrop: true,
+        },
+        {
+            anchorId: 'bundle',
+            content:
+                'Create custom bundles from the selected projects. Group related components, services, and configurations to simplify your deployment process.',
+            title: 'Create Bundles',
+            enableBackdrop: true,
+        },
+        {
+            anchorId: 'bundles',
+            content:
+                'Choose a bundle from the list and deploy your projects with a single click. Streamline your application delivery and ensure consistent deployments.',
+            title: 'Select Bundle',
+            enableBackdrop: true,
+        },
+    ];
 
     @ViewChild('displayedColumns', { read: MatSort })
     displayedColumnsMatSort: MatSort;
@@ -135,11 +147,11 @@ export class FinanceComponent implements OnInit, AfterViewInit, OnDestroy {
         'DBType',
         'actions',
     ];
-    selectedProjects: any[] = []; 
+    selectedProjects: any[] = [];
     bundles: any[] = [];
     data: any;
     private _unsubscribeAll: Subject<any> = new Subject<any>();
-//    selectedProject: any; 
+    //    selectedProject: any;
     /**
      * Constructor
      */
@@ -161,63 +173,62 @@ export class FinanceComponent implements OnInit, AfterViewInit, OnDestroy {
      * On init
      */
     ngOnInit(): void {
-      this.tourService.initialize(this.tourSteps, {});
-      const hasStartedTour = localStorage.getItem('hasStartedTour');
-      if (!hasStartedTour) {
-        setTimeout(() => {
-          this.startTour();
-          localStorage.setItem('hasStartedTour', 'true');
-        }, 100);
-      }
-      this.getProjectData();
-      this.fetchBundles();
+  
+        this.tourService.initialize(this.tourSteps, {});
+        const hasStartedTour = localStorage.getItem('hasStartedTour');
+        if (!hasStartedTour) {
+            setTimeout(() => {
+                this.startTour();
+                localStorage.setItem('hasStartedTour', 'true');
+            }, 100);
+        }
+      
+        this.getProjectData();
+        this.fetchBundles();
     }
-    
-    
+
     startTour() {
         this.tourService.start();
-    
-      }
+    }
+
     openBundleConfig(bundle): void {
         const dialogRef = this._matDialog.open(CreateDeploymentComponent, {
-          width: '1000px',
-          data: { bundle }
+            width: '1000px',
+            data: { bundle },
         });
-        this.cd.detectChanges(); 
+        this.cd.detectChanges();
 
-    
-        dialogRef.afterClosed().subscribe(result => {
-          if (result) {
-            console.log('Bundle configuration submitted:', result);
-          }
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result) {
+                console.log('Bundle configuration submitted:', result);
+            }
         });
-      }
+    }
     openBundleDialog(): void {
         this.collectSelectedProjects(); // Ensure selected projects are collected
         const dialogRef = this._matDialog.open(BundleComponent, {
-          width: '500px',
-          height: '500px',
-          data: { selectedProjects: this.selectedProjects }
+            width: '500px',
+            height: '500px',
+            data: { selectedProjects: this.selectedProjects },
         });
-        
-      
-        dialogRef.afterClosed().subscribe(result => {
-          if (result) {
-            console.log('The dialog was closed');
-            // Handle the result, e.g., save the new bundle
-          }
+
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result) {
+                console.log('The dialog was closed');
+                // Handle the result, e.g., save the new bundle
+            }
         });
-      }
-      
+    }
 
     collectSelectedProjects() {
         this.projects = this.dataSource.data;
-      this.selectedProjects = this.projects.filter(project => project.selected);
-      console.log('Projects before filtering:', this.projects);
-      console.log('Selected projects:', this.selectedProjects);
+        this.selectedProjects = this.projects.filter(
+            (project) => project.selected
+        );
+        console.log('Projects before filtering:', this.projects);
+        console.log('Selected projects:', this.selectedProjects);
     }
-    
- 
+
     editRow(element: any): void {
         this.openComposeDialog(element);
     }
@@ -225,8 +236,10 @@ export class FinanceComponent implements OnInit, AfterViewInit, OnDestroy {
     deleteRow(id: string): void {
         this.projectSerivce.deleteProject(id).subscribe(
             () => {
-                this.dataSource.data = this.dataSource.data.filter(project => project._id !== id);
-                this.cd.detectChanges(); 
+                this.dataSource.data = this.dataSource.data.filter(
+                    (project) => project._id !== id
+                );
+                this.cd.detectChanges();
                 this._snackBar.open('Project deleted successfully', 'Close', {
                     duration: 2000,
                 });
@@ -275,7 +288,7 @@ export class FinanceComponent implements OnInit, AfterViewInit, OnDestroy {
         // Open the dialog without newProjectId
         const dialogRef = this._matDialog.open(MailboxComposeComponent, {
             panelClass: 'custom-dialog-container',
-            data: { projectData }
+            data: { projectData },
         });
 
         dialogRef.componentInstance.selectedRepository.subscribe(
@@ -290,13 +303,18 @@ export class FinanceComponent implements OnInit, AfterViewInit, OnDestroy {
         dialogRef.afterClosed().subscribe((result) => {
             if (result && result.updatedProject) {
                 // Update the data source with the new/updated project
-                const index = this.dataSource.data.findIndex(project => project._id === result.updatedProject._id);
+                const index = this.dataSource.data.findIndex(
+                    (project) => project._id === result.updatedProject._id
+                );
                 if (index > -1) {
                     // Update existing project
                     this.dataSource.data[index] = result.updatedProject;
                 } else {
                     // Add new project
-                    this.dataSource.data = [...this.dataSource.data, result.updatedProject];
+                    this.dataSource.data = [
+                        ...this.dataSource.data,
+                        result.updatedProject,
+                    ];
                 }
                 this.dataSource._updateChangeSubscription(); // Trigger table update
                 this.cd.detectChanges(); // Trigger change detection manually
@@ -310,16 +328,15 @@ export class FinanceComponent implements OnInit, AfterViewInit, OnDestroy {
 
     fetchBundles(): void {
         this.financeService.getBundles().subscribe(
-          (bundles) => {
-            this.bundles = bundles;
-            this.cd.detectChanges(); // Trigger change detection manually
-          },
-          (error) => {
-            console.error('Failed to fetch bundles:', error);
-          }
+            (bundles) => {
+                this.bundles = bundles;
+                this.cd.detectChanges(); // Trigger change detection manually
+            },
+            (error) => {
+                console.error('Failed to fetch bundles:', error);
+            }
         );
-      }
-      
+    }
 
     handleUpdatedRepository(data: { provider: string }, dialogRef: any): void {
         this.financeService
